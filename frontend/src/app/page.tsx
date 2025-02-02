2a3096c92a93799f7041e771316a8e68335e6d81
ukae <k.ueta@aihub.tokyo>
2025-02-02 14:52:22 +0900

    fixed navi

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,18 +1,5 @@
-'use client'
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import { permanentRedirect } from 'next/navigation'
 
 export default function HomePage() {
-  const router = useRouter()
-
-  useEffect(() => {
-    router.replace('/login')
-  }, [router])
-
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div>Loading...</div>
-    </div>
-  )
+  permanentRedirect('/login')
 } 
