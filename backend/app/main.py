0d9c34a5e6eb91a130987f0addee58fe189c6a13
ukae <k.ueta@aihub.tokyo>
2025-02-02 17:34:36 +0900

    add config

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,11 +10,12 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",  # ローカル開発環境
-        "http://localhost:8080",  # ローカルバックエンド
-        "http://127.0.0.1:3000",
-        "http://127.0.0.1:8080",
-        "https://tech0step3app-production.up.railway.app/",  # Railway.app上の全てのサービス
+        "http://localhost:3000",     # ローカルフロントエンド
+        "http://localhost:8080",     # ローカルバックエンド
+        "http://127.0.0.1:3000",     # ローカルフロントエンド（別表記）
+        "http://127.0.0.1:8080",     # ローカルバックエンド（別表記）
+        "http://frontend:3000",      # Railway.app上のフロントエンド
+        "https://*.railway.app",     # Railway.app上の全てのサービス
     ],
     allow_credentials=True,
     allow_methods=["*"],
